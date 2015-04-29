2e0d4f08ccc344af3a7d2b97cfacc80db1c88a1a
Tyrell Cook <tyrell@aghstrategies.com>
2015-04-29 14:01:05 -0400

    init commit

---
+++ js/crm.tinymce.js
@@ -0,0 +1,154 @@
+/*_.init(function(){
+  tinyMCE.init(
+    {selector:"textarea",
+     plugins:"link image media",
+     toolbar:"link | image | media",
+     file_browser_callback:function(d,a,b,c){
+      tinyMCE.activeEditor.windowManager.open({
+        file:"/demo/kcfinder/browse.php?opener=tinymce4&field="+d+"&type="+b+"&dir="+b+"/public",
+        title:"KCFinder web file manager",
+        width:700,
+        height:500,
+        inline:true,
+        close_previous:false
+      },
+      {window:c,input:d});return false}})});
+
+      function openKCFinder(c,a,b,d){
+     tinyMCE.activeEditor.windowManager.open({
+      file:"/demo/kcfinder/browse.php?opener=tinymce&type="+b+"&dir="+b+"/public",title:"KCFinder",width:700,height:500,resizable:true,inline:true,close_previous:false,popup_css:false},{window:d,input:c});return false}
+*/
+(function($, _) {
+  function getInstance(item) {
+    var id = $(item).attr("id");
+    return tinyMCE.get(id);
+  }
+
+  function openFiles(field_name, url, type, win) {
+    console.log(field_name);
+    var browseUrl = CRM.config.userFrameworkResourceURL + "packages/kcfinder2/browse.php?opener=tinymce4&cms=civicrm&type="+type+"&dir="+url+"&field"+field_name+"&theme=default";
+    //var browseUrl = "http://dmaster.localhost/sites/default/ext/com.civicrm.tinymce/js/kcfinder/browse.php?opener=tinymce4&cms=civicrm";
+    tinyMCE.activeEditor.windowManager.open({
+        file: browseUrl,
+        title: "KCFinder Web File Manager",
+        width: 700,
+        height: 500,
+        resizable: "yes",
+        inline: true,
+        close_previous: "no",
+        popup_css: false
+    },
+    {
+      window: win,
+      input: field_name,
+    });
+    return false;
+  }
+
+  CRM.wysiwyg.supportsFileUploads = true;
+  CRM.wysiwyg.create = function(item) {
+    var id = $(item).attr("id");
+    var editor = tinymce.createEditor(id, {
+      theme : "modern",
+      plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor colorpicker textpattern"
+      ],
+      toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spellchecker separator removeformat separator ",
+      toolbar2: "print preview media | forecolor backcolor emoticons | justifyfull separator pastetext pasteword | code | fullscreen help",
+      image_advtab: true,
+      convert_urls : false,
+      remove_script_host : false,
+      file_browser_callback:   function(field_name, url, type, win) {
+        var browseUrl = CRM.config.userFrameworkResourceURL + "packages/kcfinder/browse.php?opener=tinymce4&cms=civicrm&type="+type+"&field="+field_name+"&theme=default";
+        tinyMCE.activeEditor.windowManager.open({
+            file: browseUrl,
+            title: "CiviCRM File Manager",
+            width: 700,
+            height: 500,
+            resizable: "yes",
+            inline: true,
+            close_previous: "no",
+            popup_css: false
+        },
+        {
+          window: win,
+          input: field_name,
+        });
+        return false;
+      },
+      apply_source_formatting : true,
+      setup : function(ed) {
+            var height = cj("#" + ed.editorId).attr("height");
+            $("#" + ed.editorId + "_tbl").css("height", height);
+            $("#" + ed.editorId + "_ifr").css("height", height);
+      }
+    });
+    editor.render();
+    editor.on('blur', function() {
+      editor.updateElement();
+      $(item).trigger("blur");
+      $(item).trigger("change");
+    });
+    editor.on('change', function() {
+      $(item).trigger("change");
+    });
+    editor.on('LoadContent', function() {
+      $(item).trigger("paste");
+    });
+    // tinyMCE.execCommand("mceRemoveControl", false, editor.editorId);
+    // tinyMCE.execCommand("mceAddControl"   , true,  editor.editorId);
+  }
+
+  CRM.wysiwyg.destroy = function(item) {
+    var editor = getInstance(item);
+    if (editor) {
+      editor.destroy();
+    }
+  };
+
+  CRM.wysiwyg.updateElement = function(item) {
+    var editor = getInstance(item);
+    if (editor) {
+      editor.save();
+    }
+  };
+
+  CRM.wysiwyg.getVal = function(item) {
+    var editor = getInstance(item);
+    if (editor) {
+      return editor.getContent();
+    } else {
+      return $(item).val();
+    }
+  };
+
+  CRM.wysiwyg.setVal = function(item, val) {
+    var editor = getInstance(item);
+    if (editor) {
+      return editor.setContent(val);
+    } else {
+      return $(item).val(val);
+    }
+  };
+
+  CRM.wysiwyg.insert = function(item, text) {
+    var editor = getInstance(item);
+    if (editor) {
+      editor.insertContent(text);
+    } else {
+      CRM.wysiwyg._insertIntoTextarea(item, text);
+    }
+  };
+  CRM.wysiwyg.focus = function(item) {
+    var editor = getInstance(item);
+    if (editor) {
+      editor.focus();
+    } else {
+      $(item).focus();
+    }
+  };
+
+})(CRM.$, CRM._);
---
+++ tinymce.php
@@ -0,0 +1,134 @@
+<?php
+
+require_once 'tinymce.civix.php';
+
+function tinymce_civicrm_buildForm($formName, $form) {
+  $editorID = CRM_Core_BAO_Setting::getItem(CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,
+    'editor_id'
+  );
+  $editor = CRM_Utils_Array::value($editorID,
+    CRM_Core_OptionGroup::values('wysiwyg_editor')
+  );
+  if ($editor == "TinyMCE") {
+    CRM_Core_Resources::singleton()->addScriptFile('com.civicrm.tinymce', 'js/tinymce/tinymce.min.js', 10, 'html-header');
+    CRM_Core_Resources::singleton()->addScriptFile('com.civicrm.tinymce', 'js/tinymce/jquery.tinymce.min.js', 9, 'html-header');
+    CRM_Core_Resources::singleton()->addScriptFile('com.civicrm.tinymce', 'js/crm.tinymce.js', 10, 'html-header');
+  }
+}
+
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function tinymce_civicrm_config(&$config) {
+  _tinymce_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function tinymce_civicrm_xmlMenu(&$files) {
+  _tinymce_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function tinymce_civicrm_install() {
+  return _tinymce_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function tinymce_civicrm_uninstall() {
+  return _tinymce_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function tinymce_civicrm_enable() {
+  return _tinymce_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function tinymce_civicrm_disable() {
+  return _tinymce_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function tinymce_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _tinymce_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function tinymce_civicrm_managed(&$entities) {
+  $entities[] = array(
+    'module' => 'com.civicrm.tinymce',
+    'entity' => 'OptionValue',
+    'name'  => 'TinyMCE',
+    'params' => array(
+      'version' => 3,
+      'option_group_id' => "wysiwyg_editor",
+      'label' => 'TinyMCE',
+      'is_active' => '1',
+    ),
+  );
+  return _tinymce_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function tinymce_civicrm_caseTypes(&$caseTypes) {
+  _tinymce_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function tinymce_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _tinymce_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
